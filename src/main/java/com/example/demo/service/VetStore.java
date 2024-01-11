56914fe019b6fbc55153d19135dd07856325cddc
Dragos <84362852+Gaspair@users.noreply.github.com>
2024-01-11 08:50:39 +0200

    Vet Store buildup

--- src/main/java/com/example/demo/service/VetStore.java
+++ src/main/java/com/example/demo/service/VetStore.java
@@ -1,4 +1,17 @@
 package com.example.demo.service;
 
+import com.example.demo.model.Vet;
+import org.springframework.http.ResponseEntity;
+
+import java.util.UUID;
+
 public interface VetStore {
+
+    ResponseEntity<?> getVetByID(UUID vetID);
+
+    ResponseEntity<?> getPatientsList(UUID vetID);
+
+    ResponseEntity<?> saveVet(Vet vet);
+
+    ResponseEntity<?> delete(UUID vetID);
 }
